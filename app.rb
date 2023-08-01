7b6fc84c2a436b26324655952d0688118d662801
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-08-01 00:38:46 +0000

    currency list

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require "sinatra"
-require "sinatra/reloader"
+#require "sinatra/reloader"
 
 require 'net/http'
 require 'json'
-require_relative 'get_symbols' # assuming get_symbols method is in this file
+#require_relative 'get_symbols' # assuming get_symbols method is in this file
 
 def get_symbols
   uri = URI('https://api.exchangerate.host/symbols')
@@ -11,11 +11,7 @@ def get_symbols
   JSON.parse(response)["symbols"].keys
 end
 
-get("/") do
-  "
-  <h1>Currency pairs</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get '/' do
   @currencies = get_symbols
   erb :index
 end
