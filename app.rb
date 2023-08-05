55eab085688d6864579e16d00903a6fa19c446e5
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-08-05 23:49:40 +0000

    added convert

--- app.rb
+++ app.rb
@@ -11,16 +11,30 @@ def get_symbols
   JSON.parse(response)["symbols"].keys
 end
 
-#get '/' do
-#  @currencies = get_symbols
-#  erb :index
-#end
 get '/' do
-  redirect to('/USD')
+  @currencies = get_symbols
+  erb :index
 end
+#get '/' do
+##  redirect to('/USD')
+#end
 
 get '/:currency' do
   @base_currency = params['currency'].upcase
   @other_currencies = get_symbols - [@base_currency]
   erb :conversion
+end
+
+get '/:base_currency/:target_currency' do
+  base_currency = params['base_currency'].upcase
+  target_currency = params['target_currency'].upcase
+  
+  # Make API request to get conversion rate
+  uri = URI("https://api.exchangerate.host/convert?from=#{base_currency}&to=#{target_currency}")
+  response = Net::HTTP.get(uri)
+  rate = JSON.parse(response)["result"]
+
+  @conversion_text = "1 #{base_currency} equals #{rate} #{target_currency}"
+
+  erb :convert
 end
